467b57a93461c05a40f1adeb1e5ede999078f9d7
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-02-29 21:39:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+var first, second;
+    first = document.getElementById("firstname").value;
+    second = document.getElementById("lastname").value;
 
+    document.GetElementById("here").InnerHTML = first;
+    document.GetElementById("here").InnerHTML = second;
 }
+
